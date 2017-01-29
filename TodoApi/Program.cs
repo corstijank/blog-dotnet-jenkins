8ed13e0cd4b1cc8681721ae59f01075dd81d1dae
Corstijan Kortsmit <corstijank@infosupport.com>
2017-01-29 15:08:12 +0100

    add urls to code

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -13,6 +13,7 @@ namespace TodoApi
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
+                .UseUrls("http://0.0.0.0:5000/")
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
